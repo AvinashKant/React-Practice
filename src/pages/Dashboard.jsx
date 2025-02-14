9c1012636da4f4a86d20fa15be31f61f837e631d
Avinash Kant <developer.avinashkant@gmail.com>
2025-02-14 18:45:40 +0530

    Test deploy

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import './assets/css/app.css'
+
+function Dashboard() {
+
+  return (
+    <>
+      <h1 className="text-4xl text-center text-blue-500">Hi, Vinesh</h1>
+    </>
+  )
+}
+
+export default App
