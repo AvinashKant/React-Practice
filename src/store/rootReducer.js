4022714f4204ec1e0ee4c0f4e9a220d8ff60627d
Avinash Kant <contact.avinashkantse@gmail.com>
2025-04-08 19:46:06 +0530

    Sync

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
 
-
 import { dashboardReducer } from "../module/dashboard/store/reducer";
-
 
 export default combineReducers({
   dashboard: dashboardReducer,
