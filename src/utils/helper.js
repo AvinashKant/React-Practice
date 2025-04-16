7e7c2f62b93394365626aaab5472fb815ee5ab13
Avinash Kant <contact.avinashkantse@gmail.com>
2025-04-16 16:47:36 +0530

    Async

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 export default function useArray(defaultValue) {
     const [array, setArray] = useState(defaultValue)
@@ -28,4 +28,28 @@ export default function useArray(defaultValue) {
     }
 
     return { array, set: setArray, push, filter, update, remove, clear }
+}
+
+
+
+export default function useAsync(callback, dependencies = []) {
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState()
+    const [value, setValue] = useState()
+
+    const callbackMemoized = useCallback(() => {
+        setLoading(true)
+        setError(undefined)
+        setValue(undefined)
+        callback()
+            .then(setValue)
+            .catch(setError)
+            .finally(() => setLoading(false))
+    }, dependencies)
+
+    useEffect(() => {
+        callbackMemoized()
+    }, [callbackMemoized])
+
+    return { loading, error, value }
 }
