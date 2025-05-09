51958b46090fc6d39fed16996c2a92f08075f334
Avinash Kant <contact.avinashkantse@gmail.com>
2025-05-09 19:27:37 +0530

    Sync

--- src/pages/MouseMoment.jsx
+++ src/pages/MouseMoment.jsx
@@ -1,30 +1,81 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import MouseMove from "../components/MouseMove";
 
 export default function MouseMoment() {
-  const [xPosition, setXPosition] = useState(12.1);
-  const [YPosition, setYPosition] = useState(12.1);
+    
+    const options = {
+        "o": [],
+        "A": [
+            { id: 1, name: 'Alice', age: 30 },
+            { id: 2, name: 'Bob', age: 25 },
+            { id: 3, name: 'Charlie', age: 35 }
+        ]
 
-  useEffect(() => {
-    const handleMouseMove = (event) => {
-      const id = setTimeout(() => {
-        setXPosition(event.clientX);
-        setYPosition(event.clientY);
-      }, 100);
     };
 
-    window.addEventListener("mousemove", handleMouseMove);
+    const [xPosition, setXPosition] = useState(0);
+    const [YPosition, setYPosition] = useState(0);
 
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-      clearInterval(id);
+    const [select1, setSelect1] = useState('o');
+    const [select2, setSelect2] = useState(0);
+
+    console.log(options[select1])
+    console.log(select1)
+
+
+
+    const select1Ref = useRef(null);
+    const select2Ref = useRef(null);
+
+    useEffect(() => {
+        const handleMouseMove = (event) => {
+            const id = setTimeout(() => {
+                setXPosition(event.clientX);
+                setYPosition(event.clientY);
+            }, 100);
+        };
+
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+            clearInterval(id);
+        };
+    }, []);
+
+
+    function selectBox2(event) {
+        console.log(event.target.value);
+        setSelect1(event.target.value)
+        setSelect2(event.target.value)
+
     };
-  }, []);
 
-  return (
-    <>
-      <h1>Add mouse pointer</h1>
-      <MouseMove xPosition={xPosition} YPosition={YPosition} />
-    </>
-  );
+    return (
+        <>
+            <h1>Add mouse pointer</h1>
+            <MouseMove xPosition={xPosition} YPosition={YPosition} />
+
+            <div className="bg-yellow-50">
+                <select name="" id="" ref={select1Ref} onChange={selectBox2}>
+                    <option value="0">Select option</option>
+                    <option value="A">Test1</option>
+                    <option value="2">Test2</option>
+                    <option value="3">Test2</option>
+                    <option value="4">Test3</option>
+                </select>
+            </div>
+            <div className="bg-gray-50 mt-1">
+                <select name="" id="" ref={select2Ref}>
+                    <option value="">Please select option from select 1</option>
+                    {options[select1].map((person) => (
+                        <option key={person.id} value={person.id}>
+                            {person.name} ({person.age})
+                        </option>
+                    ))}
+
+                </select>
+            </div>
+        </>
+    );
 }
