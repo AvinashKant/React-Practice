96af3adba392733d83c8660c05ef2d78f33af8b4
Avinash Kant <contact.avinashkantse@gmail.com>
2025-05-14 14:11:19 +0530

    Chips

--- src/pages/ChipsInputPage.jsx
+++ src/pages/ChipsInputPage.jsx
@@ -3,15 +3,7 @@ import ChipsInput from "../components/ChipsInput/ChipsInput";
 export default function ChipsInputPage() {
   return (
     <>
-      <div className="chip-container">
-        <div>
-          <h1>Chip input</h1>
-
-          <div>
-            <ChipsInput />
-          </div>
-        </div>
-      </div>
+      <ChipsInput />
     </>
   );
 }
