d132f62bd4b04f59751d6415043e47ce18a77c90
Avinash kant <contact.avinashkantse@gmail.com>
2025-06-14 17:21:15 +0530

    model box

--- src/pages/ModelCss.jsx
+++ src/pages/ModelCss.jsx
@@ -1,7 +1,20 @@
 export default function ModelCss() {
     return (
         <>
-            I am model box
+            <div className="flex flex-col items-center border p-2 max-w-[300px] absolute justify-center items-center">
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <p className="text-left">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita quidem, cupiditate voluptas ullam saepe temporibus odit laudantium officia doloremque ad deleniti amet, esse et. Perferendis voluptatibus ipsum itaque earum hic.
+                    </p>
+                    <div>
+                        <span className="bg-red-200 p-1 rounded-2xl">#reactjs</span>
+                        <span className="bg-red-200 m-1 p-1 rounded-2xl">#javascript</span>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
